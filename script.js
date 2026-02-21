40a46bbf9aef066ad950138975a5ff6ec7b610fb
Nadia Pérez L. <107193628+nmperezl@users.noreply.github.com>
2026-02-21 18:57:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const projects = {
   gtfs: {
     title: "Base Única de Paradas – GTFS CABA",
     image: "images/projects/paradas_gtfs2.png",
-    text: "Gerencia Operativa de Innovación y Tecnología en Transporte - MDUyT - GCBA. El desafío en este caso consistió en consolidar la información estática del sistema GTFS. En el caso de las paradas de colectivos, se encontraban distribuidas en tres bases operativas distintas: proyecto y ubicaciones legales, instalación en territorio y estimaciones derivadas del análisis de transacciones SUBE. Se realizó la unificación y estructuración de la información en una base única, documentando flujos de trabajo para articular las áreas de proyecto, instalación y fiscalización. Esta integración sirvió para generar el GTFS e integrar a los sistemas predictivos, así como para optimizar la planificación de nuevas paradas y evaluar costos de instalación.",
+    text: "Gerencia Operativa de Innovación y Tecnología en Transporte - MDUyT - GCBA. El desafío consistió en consolidar la información estática del sistema GTFS. Las paradas de colectivos estaban distribuidas en tres bases distintas (proyecto y ubicaciones legales, instalación en territorio y estimaciones a partir de transacciones SUBE). Se unificó y estructuró la información en una base única, documentando flujos de trabajo para articular áreas y asegurar consistencia. Esta integración permitió generar el GTFS, alimentar sistemas predictivos y optimizar la planificación y evaluación de nuevas paradas.",
     link: "https://datosabiertos-apis.buenosaires.gob.ar/BA_Root/Documentacion?schema_name=Transporte_3"
   },
   bariloche: {
